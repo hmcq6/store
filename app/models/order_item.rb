99346ab82cfa37a0a2bb37f9086f39dab79861db
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-11-13 01:22:54 -0500

    Fix shopping cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,8 +3,8 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validate :product_present
-  validate :order_present
+  #validate :product_present
+  #validate :order_present
 
   before_save :finalize
 
