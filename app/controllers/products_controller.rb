8f5f14cf146c3d20eb44792f190901f1c832a18b
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-11-13 18:50:04 -0500

    Lay foundation for photos on products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -52,6 +52,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :active)
+    params.require(:product).permit(:name, :price, :active, :description, :file)
   end
 end
