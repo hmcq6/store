ad727884a91c8b70bd77ff7c6359f9e8d5ee39a9
Hassan McKusick <Hassan.McKusick@gmail.com>
2017-11-26 20:48:15 -0500

    Initial work for checkout page

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'devise'
 gem 'jquery-rails'
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 
+gem 'font-awesome-sass', '~> 4.7.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [:index, :new, :create, :show, :update]
-  resources :carts, only: [:show]
+  resources :carts, only: [:show] do
+    member do
+      get :checkout
+    end
+  end
   resources :order_items, only: [:create, :update, :destroy]
 
   root to: 'products#index'
